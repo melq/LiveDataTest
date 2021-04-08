7ffd535a53a1c46a817430830a09c0ac7bbc78ab
melq <tu2sice@yahoo.co.jp>
2021-04-08 14:54:17 +0900

    :tada: initital commit

---
+++ app/src/main/java/com/melq/livedatatest/FragmentMain.kt
@@ -0,0 +1,17 @@
+package com.melq.livedatatest
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FragmentMain : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
