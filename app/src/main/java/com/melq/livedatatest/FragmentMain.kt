7f5fb3575903615fb714de9d51fba76c1d09856f
melq <tu2sice@yahoo.co.jp>
2021-04-08 15:08:35 +0900

    :construction: DataBinding を使ってレイアウトを設定した

--- app/src/main/java/com/melq/livedatatest/FragmentMain.kt
+++ app/src/main/java/com/melq/livedatatest/FragmentMain.kt
@@ -5,13 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.melq.livedatatest.databinding.FragmentMainBinding
 
 class FragmentMain : Fragment() {
+    private lateinit var binding: FragmentMainBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
