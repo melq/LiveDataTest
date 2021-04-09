dc2a274277ddd84e7288e36baec46e04b238325b
melq <tu2sice@yahoo.co.jp>
2021-04-09 13:37:39 +0900

    :construction: ViewModelクラスの作成とフィールドの作成

---
+++ app/src/main/java/com/melq/livedatatest/MainViewModel.kt
@@ -0,0 +1,26 @@
+package com.melq.livedatatest
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+class MainViewModel {
+    private val _buttonText: MutableLiveData<String> =
+            MutableLiveData<String>().also { mutableLiveData ->
+                mutableLiveData.value = "OK!!"
+            }
+    val buttonText: LiveData<String> get() = _buttonText
+
+    private val _isEnabled: MutableLiveData<Boolean> =
+            MutableLiveData<Boolean>().also { mutableLiveData ->
+                mutableLiveData.value = false
+            }
+    val isEnabled: LiveData<Boolean> get() = _isEnabled
+
+    private val _submittedText: MutableLiveData<String> =
+            MutableLiveData<String>().also { mutableLiveData ->
+                mutableLiveData.value = R.string.output_here.toString()
+            }
+    val submittedText: LiveData<String> get() = _submittedText
+
+    
+}
